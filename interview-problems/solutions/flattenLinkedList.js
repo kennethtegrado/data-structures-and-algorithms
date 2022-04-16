b097a7b6c9783e8bbb907fae3629657eb6de6814
Kenneth Tegrado <tegradok@gmail.com>
2022-04-16 14:43:53 +0800

    added comments

--- interview-problems/solutions/flattenLinkedList.js
+++ interview-problems/solutions/flattenLinkedList.js
@@ -1,8 +1,10 @@
 var flatten = function (head) {
+    // CHECK IF WE HAVE A HEAD NODE
     if (!head) return null;
     let current = head;
 
     while (current) {
+        // IF WE DO NOT HAVE A CHILD THEN WE JUST NEED TO MOVE CURRENT
         if (!current.child) {
             current = current.next;
         } else {
@@ -14,6 +16,31 @@ var flatten = function (head) {
             if (tail.next) {
                 tail.next.prev = tail;
             }
+            current.next = current.child;
+            current.next.prev = current;
+            current.child = null;
+        }
+    }
+
+    return head;
+};
+
+const remake = function (head) {
+    if (!head) return head;
+    let current = head;
+
+    while (current) {
+        if (!current.child) current = current.next;
+        // DO NOT MOVE THE POINTERS TO NEXT NODE
+        else {
+            let tail = current.child;
+
+            while (tail.next) tail = tail.next;
+
+            tail.next = current.next;
+
+            if (tail.next) tail.next.prev = tail;
+
             current.next = current.child;
             current.next.prev = current;
             current.child = null;
