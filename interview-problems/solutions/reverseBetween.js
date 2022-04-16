002491904568bef853aedec46c6b3937be4b559d
Kenneth Tegrado <tegradok@gmail.com>
2022-04-16 10:51:45 +0800

    added comments to the solutions

--- interview-problems/solutions/reverseBetween.js
+++ interview-problems/solutions/reverseBetween.js
@@ -13,6 +13,7 @@ const reverseBetween = function (head, left, right) {
     let prev = null,
         tail = currentNode;
 
+    // SWAPPING THE POINTERS
     while ((currentPos >= left) & (currentPos <= right)) {
         const next = currentNode.next;
         currentNode.next = prev;
@@ -20,9 +21,11 @@ const reverseBetween = function (head, left, right) {
         currentNode = next;
         currentPos++;
     }
+
+    // UPDATING THE VALUES OF TAIL AND START
     start.next = prev;
     tail.next = currentNode;
 
     if (left > 1) return head;
-    else return newList;
+    else return prev;
 };
